dec71439e5f0a1759de4bbe69ddd15d0555b0059
Quinton Dean <dean.quinton@ymail.com>
2022-08-20 13:40:51 -0500

    log this fortesting on netlify

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,5 @@
+console.log('production env')
+console.log(process.env.PRODUCTION_ENDPOINT)
 const DOCKER_DEV_ENDPOINT = 'http://localhost:8080/'
 // const DEV_ENDPOINT = 'http://localhost:8000/'
 const DEV_ENDPOINT = process.env.PRODUCTION_ENDPOINT || DOCKER_DEV_ENDPOINT
