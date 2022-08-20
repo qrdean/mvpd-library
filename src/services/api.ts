db4d20e437cc9e95fadd409bc6baa4416ef2aa2a
Quinton Dean <dean.quinton@ymail.com>
2022-08-20 13:26:30 -0500

    add endpoints

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 const DOCKER_DEV_ENDPOINT = 'http://localhost:8080/'
 // const DEV_ENDPOINT = 'http://localhost:8000/'
-const DEV_ENDPOINT = DOCKER_DEV_ENDPOINT
+const DEV_ENDPOINT = process.env.PRODUCTION_ENDPOINT || DOCKER_DEV_ENDPOINT
 const GO_DEV_ENDPOINT = 'http://localhost:8080/'
 export class Apis {
   constructor() {}
